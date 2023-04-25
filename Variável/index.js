83252efdabb56a3ac58bb79b173d9eaf28ec32f1
Taimisson <taimisson2003@hotmail.com>
2023-04-25 10:39:57 -0300

    JS File

--- Variável/index.js
+++ Variável/index.js
@@ -5,10 +5,19 @@ console.log("Hello World!")
 // console.log(idade)
 // alert("Legal! Você tem "+ idade +" anos!")
 
+// const nome = prompt("Qual seu nome?")
+// const endereço = prompt("Qual seu endereço?")   
+// const idade = prompt("Qual sua idade?")
+
+// console.log("Seu nome é " + nome + ", mora em " + endereço + " e tem " + idade + " anos! Legal!")
+
+// alert("Seu nome é " + nome + ", mora em " + endereço + " e tem " + idade + " anos! Legal!")
+
+
+// let anoNascimento = 2003
+// anoNascimento = 2023 - anoNascimento
+// console.log(anoNascimento)
+
 const nome = prompt("Qual seu nome?")
-const endereço = prompt("Qual seu endereço?")   
 const idade = prompt("Qual sua idade?")
-
-console.log("Seu nome é " + nome + ", mora em " + endereço + " e tem " + idade + " anos! Legal!")
-
-alert("Seu nome é " + nome + ", mora em " + endereço + " e tem " + idade + " anos! Legal!")
+console.log("Olá!", "Meu nome é",nome, "e eu tenho",idade,"anos")
