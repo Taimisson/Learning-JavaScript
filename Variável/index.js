800750bab14df21951e9a63c1dce55d88aa73882
Taimisson <taimisson2003@hotmail.com>
2023-04-25 09:32:17 -0300

    JS Study

--- Variável/index.js
+++ Variável/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+console.log("Hello World!") 
+// prompt("Qual é o seu nome? ")
+
+// const idade = prompt("Qual sua idade?")
+// console.log(idade)
+// alert("Legal! Você tem "+ idade +" anos!")
+
+const nome = prompt("Qual seu nome?")
+const endereço = prompt("Qual seu endereço?")   
+const idade = prompt("Qual sua idade?")
+
+console.log("Seu nome é " + nome + ", mora em " + endereço + " e tem " + idade + " anos! Legal!")
+
+alert("Seu nome é " + nome + ", mora em " + endereço + " e tem " + idade + " anos! Legal!")
