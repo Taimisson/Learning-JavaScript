0de61c5bcf812cbb1988b9cdd42c231ebd5aea73
Taimisson <taimisson2003@hotmail.com>
2023-04-27 10:24:39 -0300

    JS Operators

---
+++ Operadores/ex02/index.js
@@ -0,0 +1,53 @@
+// const primeiroValor = 10
+// const segundoValor = 20
+
+
+// const resultado = primeiroValor + segundoValor
+// const resultado = primeiroValor * segundoValor
+
+// console.log(resultado)
+
+// --- IDADE ---
+
+// let idade = 10
+
+// idade = idade + 20
+// idade += 20
+
+// console.log(idade)
+
+//  ex01
+
+// const num1 = 3
+// const num2 = 4
+
+// const soma = num1 + num2
+
+// console.log(soma)
+
+// ex02
+
+// const num1 = 3
+// const num2 = 5
+
+// resultado = (num1+num2) / 2
+
+// console.log(resultado)
+
+// ex03
+
+// const num1 = 5
+// const num2 = 4
+
+// resultado = (num1 - num2) * -1
+
+// console.log(resultado)
+
+// ex04
+
+const num1 = 234
+
+resto = 234 % 5
+
+console.log(resto) // Resto 4
+
