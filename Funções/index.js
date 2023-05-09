91397ce87c5ab07d57975a470745bf7a32434677
Taimisson <taimisson2003@hotmail.com>
2023-05-09 10:38:27 -0300

    JS Functions

--- Funções/index.js
+++ Funções/index.js
@@ -1,12 +1,22 @@
-function calcularArea(numero1, numero2){
-    // Aqui é uma função
-    const altura = numero1
-    const largura = numero2
-    const area = altura * largura
-    console.log(area)
+// function calcularArea(numero1, numero2){
+//     // Aqui é uma função
+//     const altura = numero1
+//     const largura = numero2
+//     const area = altura * largura
+//     console.log(area)
+// }
+
+// const primeiroNumero = Number(prompt("Digite um número"))
+// const segundoNumero = Number(prompt("Digite outro número"))
+
+// calcularArea(primeiroNumero, segundoNumero)
+
+function exibirNome(nome){
+    console.log(`Olá ${nome}`)
 }
 
-const primeiroNumero = Number(prompt("Digite um número"))
-const segundoNumero = Number(prompt("Digite outro número"))
+const nome = prompt("Digite o nome")
 
-calcularArea(primeiroNumero, segundoNumero)
+exibirNome(nome)
+exibirNome("Pedro")
+exibirNome("Lucas")
