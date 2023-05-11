44786b249b9565693c7e4d1fe8dd3948899142ff
Taimisson <taimisson2003@hotmail.com>
2023-05-11 10:22:32 -0300

    JS Object

---
+++ Objetos/index.js
@@ -0,0 +1,13 @@
+const nome = "Jose"
+const idade = 20
+const endereco = "Rua do Bobo, número 0"
+
+const usuario = {
+    nome: "Jose",
+    idade: 20,
+    endereco: "Rua do bobo, número 0"
+}
+
+usuario.profissao = "Dev"
+
+console.log(usuario.nome)
