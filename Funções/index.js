2b0256534bcc31834215f0bd01bf9739a7fcae16
Taimisson <taimisson2003@hotmail.com>
2023-05-11 08:58:23 -0300

    JS Function

--- Funções/index.js
+++ Funções/index.js
@@ -38,13 +38,63 @@
 
 
 
-function exibirPessoa(nome, idade, cidade, profissao){
-    console.log(`Eu sou ${nome}, tenho ${idade}, moro em ${cidade} e sou ${profissao}.`)
+// function exibirPessoa(nome, idade, cidade, profissao){
+//     console.log(`Eu sou ${nome}, tenho ${idade}, moro em ${cidade} e sou ${profissao}.`)
+// }
+
+// const nomePessoa = prompt("Digite o nome")
+// const idadePessoa = prompt("Digite a idade")
+// const cidadePessoa = prompt("Digite a cidade")
+// const profissaoPessoa = prompt("Digite a profissão")
+
+// exibirPessoa(nomePessoa, idadePessoa, cidadePessoa, profissaoPessoa)
+
+// function somar(n1, n2){
+//     return n1 + n2
+// }
+
+// function verificarMaior(n1, n2){
+//     return n1 > n2
+// }
+
+// function verificarPar(n1){
+//     return n1 % 2 === 0
+// }
+
+// const numero1 = Number(prompt("Digite o primeiro número"))
+// const numero2 = Number(prompt("Digite o segundo número"))
+
+// console.log(somar(numero1, numero2))
+// console.log(verificarMaior(numero1,numero2))
+// console.log(verificarPar(numero1,numero2))
+
+// function imprimirMensagem(mensagem){
+//     const tamanhoMensagem = mensagem.length
+//     const mensagemMaiuscula = mensagem.toUpperCase()
+
+//     return tamanhoMensagem, mensagemMaiuscula
+// }
+
+// console.log(imprimirMensagem("É sola da bota, e na palma da bota"))
+
+function somar(n1, n2){
+    return n1 + n2
 }
 
-const nomePessoa = prompt("Digite o nome")
-const idadePessoa = prompt("Digite a idade")
-const cidadePessoa = prompt("Digite a cidade")
-const profissaoPessoa = prompt("Digite a profissão")
+function subtrair(n1, n2){
+    return n1 - n2
+}
 
-exibirPessoa(nomePessoa, idadePessoa, cidadePessoa, profissaoPessoa)
+function multiplicar(n1, n2){
+    return n1 * n2
+}
+
+function dividir(n1, n2){
+    return n1 / n2
+}
+
+const numero1 = Number(prompt("Digite um número"))
+const numero2 = Number(prompt("Digite outro número"))
+
+console.log(`A soma é ${somar(numero1,numero2)}, \nA substração é ${subtrair(numero1,numero2)}, \nA multiplicação é ${multiplicar(numero1,numero2)}, \nA divisão é ${dividir(numero1,numero2)}`)
+
