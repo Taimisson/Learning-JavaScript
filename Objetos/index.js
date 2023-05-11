fcbaa147f6b19e3b18e142c23220a54db7fed4ae
Taimisson <taimisson2003@hotmail.com>
2023-05-11 11:04:46 -0300

    JS Objects

--- Objetos/index.js
+++ Objetos/index.js
@@ -1,13 +1,35 @@
-const nome = "Jose"
-const idade = 20
-const endereco = "Rua do Bobo, número 0"
+// const nome = "Jose"
+// const idade = 20
+// const endereco = "Rua do Bobo, número 0"
 
-const usuario = {
-    nome: "Jose",
-    idade: 20,
-    endereco: "Rua do bobo, número 0"
+// const usuario = {
+//     nome: "Jose",
+//     idade: 20,
+//     endereco: "Rua do bobo, número 0"
+// }
+
+// usuario.nome = "Name"
+// usuario.profissao = "Dev"
+
+// console.log(usuario.nome)
+
+
+// const filme = {
+//     nome: "Invocação do Mal",
+//     anoLancamento: "2013",
+//     diretor: "James Wan",
+//     elenco: ["Vera Farmiga", "Patrick Wilson", "Sterling Jerins", "Josepeh Bishara"],
+//     assistido: true
+// }
+
+// console.log(filme.nome);
+// console.log(filme.diretor);
+// console.log(filme.elenco);
+
+const pessoa = {
+    nome: "Taimisson",
+    idade: 19,
+    musica: "POP",
 }
 
-usuario.profissao = "Dev"
-
-console.log(usuario.nome)
+console.log(`O nome da pessoa é ${pessoa.nome}, ela tem ${pessoa.idade} anos e gosta muito de ${pessoa.musica}.`)
