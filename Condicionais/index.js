aefc66c505c40031660b6b7dc6a5bcb11697c142
Taimisson <taimisson2003@hotmail.com>
2023-05-15 10:43:55 -0300

    JS Conditionals

--- Condicionais/index.js
+++ Condicionais/index.js
@@ -1,6 +1,21 @@
-const num1 = 5
-const num2 = 5
+// const num1 = 5
+// const num2 = 5
 
-if (num1===num2){
-    console.log(`O primeiro número é igual ao segundo número.`)
+// if (num1===num2){
+//     console.log(`O primeiro núm é igual ao segundo número.`)
+// }
+
+function compararNumero(n1,n2){
+    if (n1 > n2){
+        console.log(`O número ${n1} é maior que o ${n2}.`)
+    }else if (n1 < n2){
+        console.log(`O número ${n1} é menor que o ${n2}.`)
+    }else if (n1 === n2){
+        console.log(`O número ${n1} é igual ao ${n2}.`)
+    }
 }
+
+const firstNumber = Number(prompt("Digite o primeiro número"))
+const secondNumber = Number(prompt("Digite o segundo número"))
+
+compararNumero(firstNumber,secondNumber)
