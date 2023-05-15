4dd6c629579505e2b894b7420e40f4ef911ab1ab
Taimisson <taimisson2003@hotmail.com>
2023-05-15 11:03:06 -0300

    JS Conditionals

--- Condicionais/index.js
+++ Condicionais/index.js
@@ -5,17 +5,34 @@
 //     console.log(`O primeiro núm é igual ao segundo número.`)
 // }
 
-function compararNumero(n1,n2){
-    if (n1 > n2){
-        console.log(`O número ${n1} é maior que o ${n2}.`)
-    }else if (n1 < n2){
-        console.log(`O número ${n1} é menor que o ${n2}.`)
-    }else if (n1 === n2){
-        console.log(`O número ${n1} é igual ao ${n2}.`)
-    }
+// function compararNumero(n1,n2){
+//     if (n1 > n2){
+//         console.log(`O número ${n1} é maior que o ${n2}.`)
+//     }else if (n1 < n2){
+//         console.log(`O número ${n1} é menor que o ${n2}.`)
+//     }else if (n1 === n2){
+//         console.log(`O número ${n1} é igual ao ${n2}.`)
+//     }
+// }
+
+// const firstNumber = Number(prompt("Digite o primeiro número"))
+// const secondNumber = Number(prompt("Digite o segundo número"))
+
+// compararNumero(firstNumber,secondNumber)
+
+
+// swtich case
+
+let paisdeOrigem
+switch(paisdeOrigem){
+    case "Inglaterra":
+        console.log("Inglês");
+    case "Brasil":
+        console.log("Brasileiro");
+    case "EUA":
+        console.log("Estadunidense");
+    case "Canadá":
+        console.log("Canadense");
+    default:
+        console.log("Nacionalidade não encontrada");
 }
-
-const firstNumber = Number(prompt("Digite o primeiro número"))
-const secondNumber = Number(prompt("Digite o segundo número"))
-
-compararNumero(firstNumber,secondNumber)
