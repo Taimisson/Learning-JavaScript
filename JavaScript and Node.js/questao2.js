061a6a9fc5dee81ef2ded07f943ae1457a8f7ee1
Taimisson <taimisson2003@hotmail.com>
2023-06-26 10:59:25 -0300

    NodeJS

--- JavaScript and Node.js/questao2.js
+++ JavaScript and Node.js/questao2.js
@@ -17,14 +17,14 @@
 const readline = require("readline");
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 rl.question("Digite um número: ", (numero) => {
-    if(number % 2 === 0){
+    if (number % 2 === 0) {
         console.log(`O número ${numero} é par.`);
-    } else{
+    } else {
         console.log(`O número ${numero} é ímpar.`);
     }
     rl.close();
