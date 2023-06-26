a11dc3ba3a5ae30ce4a7b95096151294c52a5c1c
Taimisson <taimisson2003@hotmail.com>
2023-06-26 10:50:20 -0300

    NodeJS

---
+++ JavaScript and Node.js/questao2.js
@@ -0,0 +1,31 @@
+// const readline = require("readline"); 
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// })
+
+// rl.question("Digite o primeiro número: ", (n1) => {
+//     rl.question("Digite o segundo número: ", (n2) =>{
+//         const soma = Number(n1) + Number(n2);
+//         console.log(`A soma dos números ${n1} e ${n2} é: ${soma}`);
+//         rl.close();
+//     })
+// });
+
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Digite um número: ", (numero) => {
+    if(number % 2 === 0){
+        console.log(`O número ${numero} é par.`);
+    } else{
+        console.log(`O número ${numero} é ímpar.`);
+    }
+    rl.close();
+});
