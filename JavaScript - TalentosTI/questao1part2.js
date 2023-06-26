9ab585021ba8d2729d88982551c7c713de102452
Taimisson <taimisson2003@hotmail.com>
2023-06-26 09:08:10 -0300

    JS and NodeJs

---
+++ JavaScript - TalentosTI/questao1part2.js
@@ -0,0 +1,12 @@
+const readline = require("readline"); 
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question("Digite seu nome, por favor: ", (nome) => {
+    console.log("Olá,", nome, "seja bem-vindo(a)");
+    rl.close();
+}
+)
