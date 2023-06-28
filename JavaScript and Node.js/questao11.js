01e42ddd2791ad408a0789814ccd4e27fdbfcc2a
Taimisson de Carvalho Schardosim <SchardosimTaimisson@edu.unisinos.br>
2023-06-28 08:48:41 -0300

    What's your Age?

---
+++ JavaScript and Node.js/questao11.js
@@ -0,0 +1,18 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Digite sua idade: ", (idade) => {
+    if(+idade < 18){
+        console.log("Você não pode entrar na festa.");
+    } else if (+idade >= 18){
+        console.log("Você pode entrar na festa.");
+    } else {
+        console.log("Digite uma resposta válida!");
+    } 
+
+    rl.close()
+})
