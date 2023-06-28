70cb445c864e570c2f40aa0d523b125def6c0fa8
Taimisson de Carvalho Schardosim <SchardosimTaimisson@edu.unisinos.br>
2023-06-28 11:12:06 -0300

    Calculadora de desconto

--- JavaScript and Node.js/Aula 2/questao8.js
+++ JavaScript and Node.js/Aula 2/questao8.js
@@ -5,3 +5,22 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+rl.question("Digite o valor do produto: R$ ", (valorProduto) => {
+    rl.question("Digite a porcentagem de desconto: ", (porcentagemDesconto) => {
+        const valor = parseFloat(valorProduto);
+        const desconto = parseFloat(porcentagemDesconto);
+        
+        if (isNaN(valor) || isNaN(desconto)) {
+            console.log("Valores inválidos. Por favor, insira números válidos.");
+        } else if (valor < 0 || desconto < 0) {
+            console.log("Valores inválidos. Por favor, insira valores positivos.");
+        } else {
+            const valorDesconto = (valor * desconto) / 100;
+            const valorFinal = valor - valorDesconto;
+            
+            console.log("Valor do produto com desconto: R$ " + valorFinal.toFixed(2));
+        }
+        
+        rl.close();
+    });
+});
