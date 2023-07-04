26ed1759fde38092ed7b465be99ab5c157f2e07e
Taimisson de Carvalho Schardosim <SchardosimTaimisson@edu.unisinos.br>
2023-07-04 09:36:17 -0300

    Learning TypeScript

--- JavaScript and Node.js/Aula 4/TypeScript/carro.ts
+++ JavaScript and Node.js/Aula 4/TypeScript/carro.ts
@@ -1,36 +1,39 @@
 class Carro {
-    marca: String;
-    modelo: String;
-    ano: Number;
+    marca: string;
+    modelo: string;
+    ano: number;
     ligado: boolean;
 
-    constructor(marca: String, modelo: String, ano: Number){
+    constructor(marca: string, modelo: string, ano: number) {
         this.marca = marca;
         this.modelo = modelo;
         this.ano = ano;
-    };
+    }
 
-    ligar(): void{
-        if(!this.ligado){
+    ligar(): void {
+        if (!this.ligado) {
             this.ligado = true;
             console.log("O carro foi ligado!");
         } else {
             console.log("O carro já está ligado!");
-        };
-    };
+        }
+    }
 
-    desligar(): void{
-        if(this.ligado){
+    desligar(): void {
+        if (this.ligado) {
             this.ligado = false;
             console.log("O carro foi desligado!");
         } else {
             console.log("O carro já está desligado!");
-        };
-    };
+        }
+    }
+}
 
+// const civic = new Carro("Honda", "Civic", 2022);
+// console.log(civic)
 
-
-};
-
-const civic = new Carro("Honda", "Civic", 2022);
-console.log(civic)
+const gol = new Carro("Volkswagen", "Gol", 2005);
+console.log(gol);
+gol.ligar();
+gol.ligar()
+gol.desligar();
