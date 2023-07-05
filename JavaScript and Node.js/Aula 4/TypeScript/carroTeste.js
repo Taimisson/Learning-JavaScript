1268d4c29427c8b522f0eb1eab043f180c9eb8f8
Taimisson de Carvalho Schardosim <SchardosimTaimisson@edu.unisinos.br>
2023-07-05 09:25:59 -0300

    Classe Carro

--- JavaScript and Node.js/Aula 4/TypeScript/carroTeste.js
+++ JavaScript and Node.js/Aula 4/TypeScript/carroTeste.js
@@ -2,12 +2,40 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var carro_1 = require("./carro");
 var rl = require("readline-sync");
-var marca = rl.question("Digite a marca do carro: ");
-var modelo = rl.question("Digite o modelo do carro: ");
-var ano = rl.question("Digite o ano do carro: ");
-var civic = new carro_1.Carro(marca, modelo, ano);
-console.log(civic);
-civic.ligar();
-var velocidade = rl.questionInt("Digite a velocidade que irá acelerar: ");
-civic.acelerar(velocidade);
-civic.desligar();
+var carros = [];
+var menu = true;
+var _loop_1 = function () {
+    console.log("Menu: \n  1- Construir Carro\n  2- Verificar Carro\n  0 - Sair");
+    var option = rl.question("Escolha a opção pelo número: ");
+    switch (option) {
+        case "1":
+            var marca = rl.question("Digite a marca do carro: ");
+            var modelo = rl.question("Digite o modelo do carro: ");
+            var ano = rl.question("Digite o ano do carro: ");
+            var carroNovo = new carro_1.Carro(marca, modelo, ano);
+            carros.push(carroNovo);
+            console.log("Novo carro inserido no sistema:");
+            console.log("Marca:", carroNovo.marca);
+            console.log("Modelo:", carroNovo.modelo);
+            console.log("Ano:", carroNovo.ano);
+            break;
+        case "2":
+            var carroConsulta_1 = rl.question("Qual o modelo do carro? ");
+            var carrosEncontrados = carros.filter(function (carro) { return carro.modelo === carroConsulta_1; });
+            if (carrosEncontrados.length > 0) {
+                console.log("Carros encontrados:");
+                carrosEncontrados.forEach(function (carro) { return console.log(carro); });
+            }
+            else {
+                console.log("Nenhum carro encontrado com esse modelo.");
+            }
+            break;
+        case "0":
+            console.log("Saindo...");
+            menu = false;
+            break;
+    }
+};
+while (menu === true) {
+    _loop_1();
+}
--- JavaScript and Node.js/Aula 4/TypeScript/carroTeste.ts
+++ JavaScript and Node.js/Aula 4/TypeScript/carroTeste.ts
@@ -1,18 +1,48 @@
-import { Carro } from "./carro"
+import { Carro } from "./carro";
 import * as rl from "readline-sync";
 
-const marca = rl.question("Digite a marca do carro: ");
-const modelo = rl.question("Digite o modelo do carro: ");
-const ano = rl.question("Digite o ano do carro: ");
+const carros: Carro[] = [];
+let menu: boolean = true;
 
-const civic = new Carro(marca, modelo, ano);
+while (menu === true) {
+  console.log(`Menu: 
+  1- Construir Carro
+  2- Verificar Carro
+  0 - Sair`);
 
-console.log(civic);
+  const option = rl.question("Escolha a opção pelo número: ");
 
-civic.ligar();
+  switch (option) {
+    case "1":
+      const marca = rl.question("Digite a marca do carro: ");
+      const modelo = rl.question("Digite o modelo do carro: ");
+      const ano = rl.question("Digite o ano do carro: ");
 
-const velocidade = rl.questionInt("Digite a velocidade que irá acelerar: ");
+      const carroNovo = new Carro(marca, modelo, ano);
+      carros.push(carroNovo);
 
-civic.acelerar(velocidade);
+      console.log("Novo carro inserido no sistema:");
+      console.log("Marca:", carroNovo.marca);
+      console.log("Modelo:", carroNovo.modelo);
+      console.log("Ano:", carroNovo.ano);
 
-civic.desligar();
+      break;
+
+    case "2":
+      const carroConsulta = rl.question("Qual o modelo do carro? ");
+      const carrosEncontrados = carros.filter(carro => carro.modelo === carroConsulta);
+
+      if (carrosEncontrados.length > 0) {
+        console.log("Carros encontrados:");
+        carrosEncontrados.forEach(carro => console.log(carro));
+      } else {
+        console.log("Nenhum carro encontrado com esse modelo.");
+      }
+      break;
+
+    case "0":
+      console.log("Saindo...");
+      menu = false;
+      break;
+  }
+}
