595283cd4d0c198cb1cf3e185154dda8853bbdac
Taimisson de Carvalho Schardosim <SchardosimTaimisson@edu.unisinos.br>
2023-07-07 09:08:40 -0300

    Class Calculadora

--- JavaScript and Node.js/Aula 5/Atividade 4/calculadora.js
+++ JavaScript and Node.js/Aula 5/Atividade 4/calculadora.js
@@ -1,24 +1,24 @@
 // Crie uma classe chamada "Calculadora" com métodos estáticos para realizar operações matemáticas, como "soma", "subtracao", "multiplicacao" e "divisao". Chame os métodos da classe diretamente, sem criar uma instância da classe, para realizar algumas operações matemáticas e imprimir os resultados.
-
-class Calculadora{
-
-    static soma(a: number, b: number): number{
+var Calculadora = /** @class */ (function () {
+    function Calculadora() {
+    }
+    Calculadora.soma = function (a, b) {
         return a + b;
     };
-
-    static subtracao(a: number, b: number): number{
+    ;
+    Calculadora.subtracao = function (a, b) {
         return a - b;
     };
-
-    static multiplicacao(a: number, b: number): number{
+    ;
+    Calculadora.multiplicacao = function (a, b) {
         return a * b;
     };
-
-    static divisao(a: number, b: number): number{
+    ;
+    Calculadora.divisao = function (a, b) {
         return a / b;
-    }
-}
-
+    };
+    return Calculadora;
+}());
 console.log("Soma:", Calculadora.soma(5, 5));
 console.log("Subtração:", Calculadora.subtracao(5, 5));
 console.log("Multiplicação:", Calculadora.multiplicacao(5, 5));
